314ce223c36c3c7dd55e40d64a0c1d0888025365
Niyaz Ahamad Herkal <niyaz47nhh@gmail.com>
2025-05-23 19:38:19 +0530

    added lucide react script

--- app/page.tsx
+++ app/page.tsx
@@ -38,8 +38,11 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center w-full">
       <Hero />
       <About />
-      <Services />
       <Experience />
+      <Projects />
+      <Services />
+      <TechStack />
+      <Contact />
     </main>
   );
 }
