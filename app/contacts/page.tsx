04527d32930726c1603749c787c856dc8ada2d58
Niyaz Ahamad Herkal <niyaz47nhh@gmail.com>
2025-06-12 23:52:31 +0530

    commit

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -30,7 +30,7 @@ export default function ContactPage() {
           className="flex flex-col gap-4 animate-fade-in delay-200"
           action="https://formsubmit.co/niyaz47nhh@gmail.com"
           method="POST"
-        >wdadfbfdaf as dfasd fs f
+        >wdadfbfdaf as dfasd
           <input
             type="text"
             placeholder="Your Name"
